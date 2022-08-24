3ce7fd32e6d3107c817122b9837fff325f1d8aec
james <jamesjayawara@gmail.com>
2022-08-24 20:42:44 +0700

    fixed some meta tag issues

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Hero.css'
+
+
 function Hero() {
+  const desc = 'Near Tanjung Setia International surving, 2 Km from Muhammad Taufiq Kieams Krui Airport'
   return (
     <div className='hero-section d-flex flex-column'>
         <div className='main-text'>
           <h1><b>Hotel/Resort for sale</b></h1>
-          <p>Near Tanjung Setia International surving, 2 Km from Muhammad Taufiq Kieams Krui Airport</p>
+          <meta name='description'
+            content={desc}/>
+          <p>{desc}</p>
         </div>
         <div className=' scroll-prompt d-flex flex-column justify-content-end'>
           <p className=' align-self-center'>Scroll down</p>
